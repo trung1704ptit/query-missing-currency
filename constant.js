3b3b29a77997947b90bda8f48b5777af255f2344
Trung Nguyen <trung1704ptit@gmail.com>
2023-04-21 10:21:09 +0800

    init

---
+++ constant.js
@@ -0,0 +1,157 @@
+export const uniqueSymbols = [
+  "USD",  // window server 1
+  "EUR",
+  "GBP",
+  "CAD",
+  "AUD",
+  "JPY",
+  "INR",
+// ---------------------------
+  "CNY", // done
+  "BRL",
+  "SGD",
+  "RUB",
+  "VND",
+  "KRW",
+// ------------------------  => done
+  "HKD",
+  "SEK",
+  "CHF",
+  "XAF",
+  "AFN",
+  "ALL",
+  "DZD",
+// -------------------------  => done
+  "AOA",
+  "XCD",
+  "ARS",
+  "AMD",
+  "AZN",
+  "BSD",
+  "BHD",
+// ------------------------ done
+  "BDT",
+  "BBD",
+  "BYN",
+  "BZD",
+  "XOF",
+  "BTN",
+  "BOB",
+// --------------------------  done
+  "BAM",
+  "BWP",
+  "BND",
+  "BGN",
+  "BIF",
+  "KHR",
+  "CVE",
+// ----------------------------done
+  "CLP",
+  "COP",
+  "KMF",
+  "CRC",
+  "CUP",
+  "CZK",
+// ---------------------------done
+  "CDF",
+  "DKK",
+  "DJF",
+  "DOP",
+  "EGP",
+  "SZL",
+  "ETB",
+  // ---------------------------done
+  "FJD",
+  "GMD",
+  "GEL",
+  "GHS",
+  "GTQ",
+  "GNF",
+  "GYD",
+  // --------------------------- done
+  "HTG",
+  "HNL",
+  "HUF",
+  "ISK",
+  "IDR",
+  "IRR",
+  "IQD",
+// ---------------------------
+  "ILS", // done
+  "JMD",
+  "JOD",
+  "KZT",
+  "KES",
+  "KPW",
+  "KWD",
+// ---------------------------
+  "KGS",// done
+  "LAK",
+  "LBP",
+  "LSL",
+  "LRD",
+  "LYD",
+  "MGA",
+// ---------------------------
+  "MWK",// done
+  "MYR",
+  "MVR",
+  "MRU",
+  "MUR",
+  "MXN",
+  "MDL",
+// ---------------------------
+  "MAD",// done
+  "MZN",
+  "MMK",
+  "NAD",
+  "NPR",
+  "NZD",
+  "NIO",
+// ---------------------------
+  "NGN",// done
+  "MKD",
+  "NOK",
+  "OMR",
+  "PKR",
+  "PAB",
+  "PGK",
+// ---------------------------
+  "PYG", // done
+  "PEN",
+  "PHP",
+  "PLN",
+  "QAR",
+  "RON",
+  "RWF",
+// ---------------------------
+  "SAR", // done
+  "RSD",
+  "SCR",
+  "SLL",
+  "SBD",
+  "SOS",
+  "ZAR",
+  // ---------------------------
+  "LKR", // window server 3
+  "SDG",
+  "SRD",
+  "TWD",
+  "TJS",
+  "TZS",
+  "THB",
+// ---------------------------
+  "TOP",  // window 4 running
+  "TTD",
+  "TND",
+  "TRY",
+  "TMT",
+  "UGX",
+  "UAH",
+  // ---------------------------
+  "AED", // my tinh trong phong
+  "UYU",
+  "UZS",
+  "YER",
+  "ZMW",
+];
---
+++ index.js
@@ -0,0 +1,193 @@
+const fs = require('fs');
+const axios = require('axios');
+
+const symbolsList = [
+  'USD',
+  'EUR',
+  'GBP',
+  'CAD',
+  'AUD',
+  'JPY',
+  'INR',
+  'CNY',
+  'BRL',
+  'SGD',
+  'RUB',
+  'VND',
+  'KRW',
+  'HKD',
+  'SEK',
+  'CHF',
+  'XAF',
+  'AFN',
+  'ALL',
+  'DZD',
+  'AOA',
+  'XCD',
+  'ARS',
+  'AMD',
+  'AZN',
+  'BSD',
+  'BHD',
+  'BDT',
+  'BBD',
+  'BYN',
+  'BZD',
+  'XOF',
+  'BTN',
+  'BOB',
+  'BAM',
+  'BWP',
+  'BND',
+  'BGN',
+  'BIF',
+  'KHR',
+  'CVE',
+  'CLP',
+  'COP',
+  'KMF',
+  'CRC',
+  'CUP',
+  'CZK',
+  'CDF',
+  'DKK',
+  'DJF',
+  'DOP',
+  'EGP',
+  'SZL',
+  'ETB',
+  'FJD',
+  'GMD',
+  'GEL',
+  'GHS',
+  'GTQ',
+  'GNF',
+  'GYD',
+  'HTG',
+  'HNL',
+  'HUF',
+  'ISK',
+  'IDR',
+  'IRR',
+  'IQD',
+  'ILS',
+  'JMD',
+  'JOD',
+  'KZT',
+  'KES',
+  'KPW',
+  'KWD',
+  'KGS',
+  'LAK',
+  'LBP',
+  'LSL',
+  'LRD',
+  'LYD',
+  'MGA',
+  'MWK',
+  'MYR',
+  'MVR',
+  'MRU',
+  'MUR',
+  'MXN',
+  'MDL',
+  'MAD',
+  'MZN',
+  'MMK',
+  'NAD',
+  'NPR',
+  'NZD',
+  'NIO',
+  'NGN',
+  'MKD',
+  'NOK',
+  'OMR',
+  'PKR',
+  'PAB',
+  'PGK',
+  'PYG',
+  'PEN',
+  'PHP',
+  'PLN',
+  'QAR',
+  'RON',
+  'RWF',
+  'SAR',
+  'RSD',
+  'SCR',
+  'SLL',
+  'SBD',
+  'SOS',
+  'ZAR',
+  'LKR',
+  'SDG',
+  'SRD',
+  'TWD',
+  'TJS',
+  'TZS',
+  'THB',
+  'TOP',
+  'TTD',
+  'TND',
+  'TRY',
+  'TMT',
+  'UGX',
+  'UAH',
+  'AED',
+  'UYU',
+  'UZS',
+  'YER',
+  'ZMW',
+];
+
+const query = async (iso) => {
+  try {
+    let { data } = await axios.get(
+      `http://38.242.203.248:5000/api/v1/currency/rates?from=${iso}`,
+    );
+    if (data) {
+      return data.rates;
+    }
+    return null;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+const checking = async () => {
+  try {
+    const results = [];
+    const symbols = [...new Set(symbolsList)];
+    for(item of symbols) {
+      console.log('=>>> ', item)
+      let rates = await query(item);
+      if (rates) {
+        rates = rates.map(r => r.to);
+        const filterNotExist = symbols.filter(s => !rates.includes(s));
+        if (filterNotExist.length > 0) {
+          filterNotExist.forEach(f => {
+            if (item !== f) {
+              results.push(`https://www.google.com/finance/quote/${item}-${f}`);
+            }
+          })
+        }
+      } else {
+        console.log('empty currency:', item);
+      }
+    };
+
+    fs.writeFile("output.json", JSON.stringify({ results }, null, 2) , 'utf8', function (err) {
+      if (err) {
+      console.log("An error occured while writing JSON Object to File.");
+          return console.log(err);
+      }
+  
+      console.log("JSON file has been saved.");
+  });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+checking();
